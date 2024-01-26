e2f4937badeda7ac2fd8d102df392b232aa60d0d
Roman Sachuck <roman.sachuk.1997@mail.ru>
2024-01-26 17:22:58 +0300

    .

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Practices
+{
+    public class Class1
+    {
+        [Test]
+        public void Subtraction_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Subtraction(300, 10) == 290);
+        }
+
+        [Test]
+        public void Additional_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Additional(300, 10) == 310);
+        }
+
+        [Test]
+        public void Miltiplication_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Miltiplication(300, 10) == 3000);
+        }
+
+        [Test]
+        public void Division_MustReturnCorrectValue()
+        {
+            Calculator calculator = new Calculator();
+            Assert.That(calculator.Division(300, 10) == 30);
+        }
+    }
+}
+
